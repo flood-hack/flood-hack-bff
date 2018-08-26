b461fd778b440d3557ba716e2bf9d11b8470a6ef
Bobby Earl <bobby.earl@blackbaud.com>
2018-08-26 13:01:11 -0400

    Added social

---
+++ Models/SocialSettings.cs
@@ -0,0 +1,11 @@
+namespace flood_hackathon.Models
+{
+    public class SocialSettings
+    {
+        public string AccessToken { get; set; }
+        public string AccessTokenSecret { get; set; }
+        public string ConsumerKey { get; set; }
+        public string ConsumerSecret { get; set; }
+        public string URI { get; set; }
+    }
+}
